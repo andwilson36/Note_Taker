fa4e6eeab9277da6ec78439bd142606acea1fba4
Andrew Wilson <Wilsonandrew57@gmail.com>
2021-07-21 17:45:10 -0500

    made server

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+// PORT number
+const PORT = 3001;
+
+// express
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
